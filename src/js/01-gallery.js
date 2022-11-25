f9c879d215721a5d22b4c3cec893d9c35d8b78bd
onluwin <nikitagurzhiy1@gmail.com>
2022-11-25 23:33:35 +0300

    .

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,8 @@
-// Add imports above this line
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
 
 console.log(galleryItems);
