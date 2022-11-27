d2f3547d03c492c0bbb4cec0aae40f1278721448
onluwin <105060163+onluwin@users.noreply.github.com>
2022-11-27 14:04:42 +0000

    Deploying to gh-pages from @ onluwin/goit-js-hw-08@a828abab051361210c8c616c0f123fdaff03ff30 🚀

---
+++ 03-feedback.a34866ee.js
@@ -0,0 +1,2 @@
+const e=document.querySelector('input[name="email"]'),t=document.querySelector('textarea[name="message"]'),n=(document.querySelector("form"),function(e){new FormData;console.log(e.currentTarget.value)});e.addEventListener("input",n),t.addEventListener("input",n),console.log(e,t);
+//# sourceMappingURL=03-feedback.a34866ee.js.map
