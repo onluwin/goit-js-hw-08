d3273258743e7eb5fe14ab0119e302076ed0e7f4
onluwin <nikitagurzhiy1@gmail.com>
2022-11-30 23:03:19 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,16 +1,55 @@
-// const emailFormRef = document.querySelector('input[name="email"]');
-// const messageFormRef = document.querySelector('textarea[name="message"]');
-// const formRef = document.querySelector('form');
-// const LOCAL_STORAGE_DATA_KEY = 'feedback-form-state'
+const emailFormRef = document.querySelector('input[name="email"]');
+const messageFormRef = document.querySelector('textarea[name="message"]');
+const formRef = document.querySelector('form');
+const LOCAL_STORAGE_DATA_KEY = 'feedback-form-state';
+
+let formData = {}
+initPage()
+
+// console.log(saveData);
 
 
-// const onFormInput = function (e) {
-//     let formData = {}
-//     const { name, value } = e.target
-//     formData[name] = value
-//     // console.log(formData);
-// }
-// messageFormRef.addEventListener('input', onFormInput)
-// emailFormRef.addEventListener('input', onFormInput)
-// // console.log(messageFormRef);
-console.log('ff');
+
+
+
+
+
+    
+    // console.log(saveData);
+    
+    
+const onFormInput = function (e) {
+
+    const { name, value } = e.currentTarget
+    formData[name] = value
+    const strgingifyFormData = JSON.stringify(formData);
+    
+    console.log('formData' , formData);
+    console.log('saveData, ', saveData);
+    
+    console.log('strgingifyFormData', strgingifyFormData);
+    console.log('-------------');
+    
+    localStorage.setItem(LOCAL_STORAGE_DATA_KEY, strgingifyFormData)
+    
+    
+    
+    
+}
+const saveData = localStorage.getItem(LOCAL_STORAGE_DATA_KEY);
+function initPage() {
+    if (!saveData) {
+        return;
+    }
+    else {
+        emailFormRef.value = JSON.parse(saveData).email
+    }
+}
+
+
+
+
+messageFormRef.addEventListener('input', onFormInput)
+emailFormRef.addEventListener('input', onFormInput)
+// console.log(messageFormRef);
+// console.log('ff');
