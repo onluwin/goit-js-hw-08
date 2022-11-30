8f51b1e3c2d89d32f9935097abb61a04e9f54351
onluwin <nikitagurzhiy1@gmail.com>
2022-11-30 21:07:40 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,14 +1,16 @@
-const emailFormRef = document.querySelector('input[name="email"]');
-const messageFormRef = document.querySelector('textarea[name="message"]');
-const formRef = document.querySelector('form');
+// const emailFormRef = document.querySelector('input[name="email"]');
+// const messageFormRef = document.querySelector('textarea[name="message"]');
+// const formRef = document.querySelector('form');
+// const LOCAL_STORAGE_DATA_KEY = 'feedback-form-state'
 
 
-const onFormInput = function (e) {
-    const formData = new FormData()
-    console.log(e.currentTarget.value);
-}
-emailFormRef.addEventListener('input',onFormInput)
-messageFormRef.addEventListener('input',onFormInput)
-
-
-console.log(emailFormRef, messageFormRef);
+// const onFormInput = function (e) {
+//     let formData = {}
+//     const { name, value } = e.target
+//     formData[name] = value
+//     // console.log(formData);
+// }
+// messageFormRef.addEventListener('input', onFormInput)
+// emailFormRef.addEventListener('input', onFormInput)
+// // console.log(messageFormRef);
+console.log('ff');
