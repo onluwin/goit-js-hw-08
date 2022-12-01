30f8560830733f204f6d55541e684442ddc3c80e
onluwin <nikitagurzhiy1@gmail.com>
2022-12-01 18:04:42 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,53 +3,22 @@ const messageFormRef = document.querySelector('textarea[name="message"]');
 const formRef = document.querySelector('form');
 const LOCAL_STORAGE_DATA_KEY = 'feedback-form-state';
 
-let formData = {}
 initPage()
 
-// console.log(saveData);
-
-
-
-
-
-
-
-    
-    // console.log(saveData);
-    
-    
-const onFormInput = function (e) {
-
-    const { name, value } = e.currentTarget
-    formData[name] = value
-    const strgingifyFormData = JSON.stringify(formData);
-    
-    console.log('formData' , formData);
-    console.log('saveData, ', saveData);
-    
-    console.log('strgingifyFormData', strgingifyFormData);
-    console.log('-------------');
-    
-    localStorage.setItem(LOCAL_STORAGE_DATA_KEY, strgingifyFormData)
-    
-    
-    
-    
+let formData = {}
+function onFormInput(e) {
+    const { name, value } = e.target;
+    formData[name] = value;
+    localStorage.setItem(LOCAL_STORAGE_DATA_KEY, JSON.stringify(formData))
 }
-const saveData = localStorage.getItem(LOCAL_STORAGE_DATA_KEY);
-function initPage() {
+
+function initPage(e) {
+    const saveData = localStorage.getItem(LOCAL_STORAGE_DATA_KEY)
     if (!saveData) {
-        return;
-    }
-    else {
-        emailFormRef.value = JSON.parse(saveData).email
+        return
+    } else {
+        e.currentTarget.value = JSON.parse(saveData).email
     }
 }
-
-
-
-
 messageFormRef.addEventListener('input', onFormInput)
 emailFormRef.addEventListener('input', onFormInput)
-// console.log(messageFormRef);
-// console.log('ff');
